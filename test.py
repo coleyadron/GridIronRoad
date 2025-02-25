7b13bceb0980f45a41416a30eb1ab56a9787107d
Cole <cole.yadron@gmail.com>
2025-02-25 12:00:54 -0600

    tested pygames functionality

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
 ##This is a test file
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000, 800))
+
+##Sets Font 
+font = pygame.font.Font("assets/Fonts/MinecraftRegular-Bmg3.otf", 20)
+
+img = pygame.image.load("assets/images/gridIronLogo.PNG").convert()
+
+##Renders Text
+textSurface = font.render("Start Game", True, (255, 255, 255))
+
+screen.blit(textSurface, (400, 300))
+screen.blit(img, (0, 0))
+
+pygame.display.update()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
