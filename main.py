f4d8fa87f7fd7b4388ff7dd92ba628474add2a74
lorisoj-ThinkPad <jgloriso@gmail.com>
2025-02-26 15:42:17 -0600

    Icon, key inputs

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame, sys
+import experienceSelection
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.init()
+
+info = pygame.display.Info()
+screen = pygame.display.set_mode((1400, 1050))
+
+pygame.display.set_caption("GridIron Road")
+pygame.display.set_icon(pygame.image.load("assets/images/gridIronLogo.PNG"))
+
+##Sets Font 
+font = pygame.font.Font("assets/Fonts/MinecraftRegular-Bmg3.otf", 35)
+
+img = pygame.image.load("assets/images/gridIronLogo.PNG").convert()
+
+##Renders Text
+textSurface = font.render("Press SPACE to Start Game", True, (255, 255, 255))
+
+screen.blit(textSurface, (500, 725))
+screen.blit(img, (360, 100))
+
+pygame.display.update()
+
+running = True
+
+def kill_game():
+    running = False
+    pygame.quit()
+    sys.exit()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            kill_game()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                print("Game has been closed")
+                kill_game()
+
+            if event.key == pygame.K_SPACE:
+                print("SPACE action, next screen")
+                
+
+pygame.quit()
