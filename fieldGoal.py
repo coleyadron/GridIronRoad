f7e7d9205d099b36c5e5ddc49732664dc8233955
Cole <cole.yadron@gmail.com>
2025-02-28 13:58:38 -0600

    WIP field goal charge

--- fieldGoal.py
+++ fieldGoal.py
@@ -6,3 +6,22 @@ def kill_game():
     pygame.quit()
     sys.exit()
 
+def fieldGoal(screen):
+    screen.fill((0, 0, 0))
+
+    font = pygame.font.Font("assets/Fonts/MinecraftRegular-Bmg3.otf", 35)
+
+    pygame.display.update()
+
+    width = 0
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                kill_game()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print("charging kick")
+                    width += 1
+
+    pygame.quit() 
