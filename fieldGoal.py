86979b156c1a105ff3c1e1e01edfad70893d1a35
Cole <cole.yadron@gmail.com>
2025-02-28 14:58:03 -0600

    added moving bar with space

--- fieldGoal.py
+++ fieldGoal.py
@@ -10,18 +10,43 @@ def fieldGoal(screen):
     screen.fill((0, 0, 0))
 
     font = pygame.font.Font("assets/Fonts/MinecraftRegular-Bmg3.otf", 35)
+    instruction = font.render("Hold 'SPACE' to charge kick ", True, (255, 255, 255))
+    screen.blit(instruction, (0, 600))
 
     pygame.display.update()
 
     width = 0
+    confirm = False
+    switch = False
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 kill_game()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    print("charging kick")
-                    width += 1
+            
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and not confirm:
+            if width <= 900 and switch == False:
+                width += 2
+                if width == 900:
+                    switch = True
+                    width -= 1
+                    print("Switched True")
+            elif switch == True:
+                width -= 3 
+                if width == 1:
+                    switch = False
+                    print("Switched False")
+
+         
+        pygame.draw.rect(screen, (255, 0, 0), (0, 650, width, 50))
+        pygame.display.update()
 
     pygame.quit() 
+
+def main():
+    pygame.init()
+    pygame.display.init()
+    fieldGoal(screen = pygame.display.set_mode((1400, 1050)))
+
+main()
