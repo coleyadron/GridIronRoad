d5d4db4c7c066b73348ef0e17c9f513dd6c21798
Cole <cole.yadron@gmail.com>
2025-02-28 15:02:26 -0600

    bugged field goal back

--- fieldGoal.py
+++ fieldGoal.py
@@ -26,20 +26,21 @@ def fieldGoal(screen):
             
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and not confirm:
-            if width <= 900 and switch == False:
+            if width <= 1000 and switch == False:
                 width += 2
-                if width == 900:
+                if width == 1000:
                     switch = True
                     width -= 1
                     print("Switched True")
-            elif switch == True:
+            if switch == True:
+                print("go back ")
                 width -= 3 
                 if width == 1:
                     switch = False
                     print("Switched False")
 
          
-        pygame.draw.rect(screen, (255, 0, 0), (0, 650, width, 50))
+        pygame.draw.rect(screen, (255, 0, 0), (50, 650, width, 50))
         pygame.display.update()
 
     pygame.quit() 
