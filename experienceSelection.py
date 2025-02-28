e6fe20fec011a254f03ac8f2f6ba1e0dbe1b0b54
lorisoj-ThinkPad <jgloriso@gmail.com>
2025-02-28 12:53:32 -0600

    show input on screen

--- experienceSelection.py
+++ experienceSelection.py
@@ -26,10 +26,6 @@ def selectExperience(screen):
 
     pygame.display.update()
 
-    start_Selection(screen)
-
-
-def start_Selection(screen):
     experienceOpen = True
     confirmSelection = False
     while experienceOpen:
@@ -40,12 +36,39 @@ def start_Selection(screen):
                 if event.key == pygame.K_1 or event.key == pygame.K_KP1 and not confirmSelection:
                     print("Experience 1 selected")
                     confirmSelection = True
+
+                    inputSelection = font.render("Select your experience: 1", True, (255, 255, 255))
+                    screen.fill((0, 0, 0))
+                    screen.blit(experience1, (0, 0))
+                    screen.blit(experience2, (0, 50))
+                    screen.blit(experience3, (0, 100))
+                    screen.blit(inputSelection, (0, screen.get_height() - inputSelection.get_height() - 75))
+                    pygame.display.update()
+
                 elif event.key == pygame.K_2  or event.key == pygame.K_KP2 and not confirmSelection:
                     print("Experience 2 selected")
                     confirmSelection = True
+
+                    inputSelection = font.render("Select your experience: 2", True, (255, 255, 255))
+                    screen.fill((0, 0, 0))
+                    screen.blit(experience1, (0, 0))
+                    screen.blit(experience2, (0, 50))
+                    screen.blit(experience3, (0, 100))
+                    screen.blit(inputSelection, (0, screen.get_height() - inputSelection.get_height() - 75))
+                    pygame.display.update()
+
                 elif event.key == pygame.K_3 or event.key == pygame.K_KP3 and not confirmSelection:
                     print("Experience 3 selected")
                     confirmSelection = True
+
+                    inputSelection = font.render("Select your experience: 3", True, (255, 255, 255))
+                    screen.fill((0, 0, 0))
+                    screen.blit(experience1, (0, 0))
+                    screen.blit(experience2, (0, 50))
+                    screen.blit(experience3, (0, 100))
+                    screen.blit(inputSelection, (0, screen.get_height() - inputSelection.get_height() - 75))
+                    pygame.display.update()
+
                 if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER and confirmSelection:
                     experienceOpen = False
                     print("Enter action, next screen")
@@ -57,4 +80,10 @@ def start_Selection(screen):
                     confirmSelection = False
                     print("Backspace action, reselect experience")
 
-                    
+                    inputSelection = font.render("Select your experience:", True, (255, 255, 255))
+                    screen.fill((0, 0, 0))
+                    screen.blit(experience1, (0, 0))
+                    screen.blit(experience2, (0, 50))
+                    screen.blit(experience3, (0, 100))
+                    screen.blit(inputSelection, (0, screen.get_height() - inputSelection.get_height() - 75))
+                    pygame.display.update()
