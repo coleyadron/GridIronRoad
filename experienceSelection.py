0d6b05c6e7f58a1f5c1a871d4a1e880c2873a4a2
Cole <cole.yadron@gmail.com>
2025-02-28 12:19:07 -0600

    Added text selection highlights and allowed to flip through options

--- experienceSelection.py
+++ experienceSelection.py
@@ -33,13 +33,40 @@ def selectExperience(screen):
             if event.type == pygame.QUIT:
                 kill_game()
             if event.type == pygame.KEYDOWN and experienceOpen:
-                if event.key == pygame.K_1 and not confirmSelection:
+                if event.key == pygame.K_1:
+                    ##Re-Renders text to show selection in red
+                    experience1 = font.render("Experience 1", True, (255, 0, 0))
+                    experience2 = font.render("Experience 2", True, (255, 255, 255))
+                    experience3 = font.render("Experience 3", True, (255, 255, 255))
+
+                    screen.blit(experience1, (500, 725))
+                    screen.blit(experience2, (500, 775))
+                    screen.blit(experience3, (500, 825))
+                    pygame.display.update()
                     print("Experience 1 selected")
                     confirmSelection = True
-                elif event.key == pygame.K_2 and not confirmSelection:
+                elif event.key == pygame.K_2:
+                    ##Re-Renders text to show selection in red
+                    experience1 = font.render("Experience 1", True, (255, 255, 255))
+                    experience2 = font.render("Experience 2", True, (255, 0, 0))
+                    experience3 = font.render("Experience 3", True, (255, 255, 255))
+
+                    screen.blit(experience1, (500, 725))
+                    screen.blit(experience2, (500, 775))
+                    screen.blit(experience3, (500, 825))
+                    pygame.display.update()
                     print("Experience 2 selected")
                     confirmSelection = True
-                elif event.key == pygame.K_3 and not confirmSelection:
+                elif event.key == pygame.K_3:
+                    ##Re-Renders text to show selection in red
+                    experience1 = font.render("Experience 1", True, (255, 255, 255))
+                    experience2 = font.render("Experience 2", True, (255, 255, 255))
+                    experience3 = font.render("Experience 3", True, (255, 0, 0))
+
+                    screen.blit(experience1, (500, 725))
+                    screen.blit(experience2, (500, 775))
+                    screen.blit(experience3, (500, 825))
+                    pygame.display.update()
                     print("Experience 3 selected")
                     confirmSelection = True
                 if event.key == pygame.K_RETURN and confirmSelection:
