282a1e29bc5ebb803db2e885c552dc710032fa3f
Cole <cole.yadron@gmail.com>
2025-03-03 16:20:11 -0600

    moved assests down

--- fieldGoal.py
+++ fieldGoal.py
@@ -12,13 +12,13 @@ def fieldGoal(screen):
 
     font = pygame.font.Font("assets/Fonts/MinecraftRegular-Bmg3.otf", 35)
     instruction = font.render("Hold 'SPACE' to charge kick. Press 'SPACE' to enter kick", True, (255, 255, 255))
-    screen.blit(instruction, (0, 600))
+    screen.blit(instruction, (0, 800))
 
-    pygame.draw.rect(screen, (45, 45, 45), (50, 650, 1050, 50))
+    pygame.draw.rect(screen, (45, 45, 45), (50, 850, 1050, 50))
     #Draws Space of charge bar
     success = random.randint(450, 800)
     print(success)
-    pygame.draw.rect(screen, (255, 0, 0), (success, 650, 150, 50))
+    pygame.draw.rect(screen, (255, 0, 0), (success, 850, 150, 50))
     pygame.display.update()
 
     width = 0
@@ -59,10 +59,10 @@ def fieldGoal(screen):
 
             
         screen.fill((0, 0, 0))
-        screen.blit(instruction, (0,600))
-        pygame.draw.rect(screen, (45, 45, 45), (50, 650, 1050, 50))
-        pygame.draw.rect(screen, (255, 255, 0), (success, 650, 100, 50))
-        pygame.draw.rect(screen, (255, 0, 0), (50, 650, width, 50))
+        screen.blit(instruction, (50,800))
+        pygame.draw.rect(screen, (45, 45, 45), (50, 850, 1050, 50))
+        pygame.draw.rect(screen, (255, 255, 0), (success, 850, 100, 50))
+        pygame.draw.rect(screen, (255, 0, 0), (50, 850, width, 50))
         pygame.display.update() 
 
     pygame.quit() 
